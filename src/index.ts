b30fe13f5b41a49b9fe47b415d589894175f30f2
agumy <nov13sh@gmail.com>
2021-05-05 23:23:55 +0900

    [wip] fetch dom lsit for repositories

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ import puppeteer from "puppeteer";
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://github.com/trending/javascript?since=daily");
+  const repositoryDOMList = await page.$$(".Box-row");
+  console.log(repositoryDOMList);
 })();
